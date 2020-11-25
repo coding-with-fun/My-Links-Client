0940782eede1f5c9ac425cf264e34093329c5196
Arc Coder | Harsh Patel <dev@arccoder.in>
2020-11-25 22:02:40 +0530

    Created components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { useContext } from "react";
 import { UserContext } from "./context/UserContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Admin from "./components/Admin/Admin";
+import User from "./components/User/User";
 
 const App = () => {
     const { user } = useContext(UserContext);
 
     return (
         <div className="app__container">
-            <h1>Hello {user.name}</h1>
+            <Router>
+                <h1>Hello {user.name}</h1>
+
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/admin" component={Admin} />
+                    <Route path="/:user" component={User} />
+                </Switch>
+            </Router>
         </div>
     );
 };
